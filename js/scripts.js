c445ac0e048ee6068b33a2659eba8de0004c2c20
Ryan Mathisen and Rafael Perez <ryan@>
2016-10-20 14:03:18 -0700

    add some jQuery to manage list selections, still a work in progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,35 @@
 $(document).ready(function (){
 
-    $("form#userInput").submit(event){
+    var age = parseInt($("#age").val());
+    var gender = $("#gender").val();
+    var hobby1 = $("#hobby1").val();
+    var hobby2 = $("#hobby2").val();
+    var tempValue = "";
 
-      
+    $("form#userInput").submit(function(event){
+
+      console.log(hobby1);
       event.preventDefault();
-    }
+    });
+
+    $("#hobby1").click(function() {
+      tempValue = hobby1;
+      $("#hobby2 ."+hobby1).show();
+    }).change(function(){
+        hobby1 = $(this).val().toLowerCase();
+        console.log($(this).val());
+        $("#hobby2 ."+hobby1).hide();
+        return hobby1;
+        // console.log("function1");
+    }, function() {
+      if (tempValue===hobby1) {
+        $("#hobby2 ."+hobby1).hide();
+      }
+    });
 
 });
+
+// function {
+//   find opposite list
+//   hide the element selected by list
+// }
