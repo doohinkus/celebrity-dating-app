f3a87434502a9c3a1899894c009314ee6a241c31
Ryan Mathisen and Rafael Perez <rafael@>
2016-10-20 16:54:54 -0700

    attempt to solve checkbox selection issue.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,42 @@
 $(document).ready(function (){
-
-    var age = parseInt($("#age").val());
-    var gender = $("#gender").val();
-    var hobby1 = $("#hobby1").val();
-    var hobby2 = $("#hobby2").val();
-    var tempValue = "";
-
     $("form#userInput").submit(function(event){
-
-      console.log(hobby1);
       event.preventDefault();
-    });
+      var age = parseInt($("#age").val());
+      var gender = $("#gender").val();
+      var hobby1 = $("#hobby1").val();
+      var hobby2 = $("#hobby2").val();
+      var selection = getCheckedCheckboxesFor('hobby')
 
-    $("#hobby1").click(function() {
-      tempValue = hobby1;
-      $("#hobby2 ."+hobby1).show();
-    }).change(function(){
-        hobby1 = $(this).val().toLowerCase();
-        console.log($(this).val());
-        $("#hobby2 ."+hobby1).hide();
-        return hobby1;
-        // console.log("function1");
-    }, function() {
-      if (tempValue===hobby1) {
-        $("#hobby2 ."+hobby1).hide();
+      if ((gender === "Male") && (selection.indexOf("Photography")===0)) {
+        $("#ricci").show();
       }
     });
 
+    function getCheckedCheckboxesFor(checkboxName) {
+    var checkboxes = document.querySelectorAll('input[name="' + checkboxName + '"]:checked'), values = [];
+    Array.prototype.forEach.call(checkboxes, function(el) {
+        values.push(el.value);
+    });
+    // console.log(values);
+    }
+    var array = [2, 7, 7, 2];
+    console.log(array.indexOf(2))
 });
 
-// function {
-//   find opposite list
-//   hide the element selected by list
-// }
+
+
+
+
+
+
+
+
+// $("#hobby1").click(function() {
+//   tempValue = hobby1;
+//   $("#hobby2 ."+hobby1).show();
+// }).change(function(){
+//     hobby1 = $(this).val().toLowerCase();
+//     // console.log("function 1!!");
+//     $("#hobby2 ."+hobby1).hide();
+//     return hobby1;
+// });
